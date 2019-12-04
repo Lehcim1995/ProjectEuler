ca8e462d4c3e96d184cb97754f3d661ba22017de
Jacobs <Michel.Jacobs@philips.com>
2019-12-04 10:00:57 +0100

    day 4

---
+++ adventofcode2019/Days/Day4.cs
@@ -0,0 +1,67 @@
+﻿using adventofcode2019.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2019.Days
+{
+    public class Day4 : IProblem
+    {
+        int input1 = 240920;
+        int input2 = 789857;
+
+        private bool passCriteria(int pass)
+        {
+            int last = -1;
+            bool hasDouble = false;
+
+            for(int i = 0; i < 6; i++)
+            {
+                int curr = getNumberOn(pass, i);
+
+                if (curr < last)
+                {
+                    return false;
+                }
+
+                if (curr == last)
+                {
+                    hasDouble = true;
+                }
+
+                last = curr;
+
+            }
+            return hasDouble;
+        }
+
+        private int getNumberOn(int input, int num)
+        {
+            String s = input + "";
+
+            if (num > s.Length)
+            {
+                // error
+            }
+
+            return int.Parse(s[num] + "");
+        }
+
+        public long Answer(params long[] arguments)
+        {
+            int awnser = 0;
+
+            for(int i = input1; i < input2; i++)
+            {
+                if (passCriteria(i))
+                {
+                    awnser++;
+                }
+            }
+
+            return awnser;
+        }
+    }
+}
---
+++ adventofcode2019/Days/Day4_4.cs
@@ -0,0 +1,87 @@
+﻿using adventofcode2019.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2019.Days
+{
+    public class Day4_4 : IProblem
+    {
+        int input1 = 240920;
+        int input2 = 789857;
+
+        private bool passCriteria(int pass)
+        {
+            int last = -1;
+            bool hasDouble = false;
+            Dictionary<int, int> repeat = new Dictionary<int, int>();
+
+            for(int i = 0; i < 6; i++)
+            {
+                int curr = getNumberOn(pass, i);
+
+                if (curr < last)
+                {
+                    return false;
+                }
+
+                if (curr == last)
+                {
+                    if (repeat.ContainsKey(curr))
+                    {
+                        repeat[curr] += 1;
+                    }
+                    else
+                    {
+                        repeat.Add(last, 2);
+                    }
+                    
+                }
+
+                last = curr;
+
+            }
+
+            foreach(var r in repeat)
+            {
+                if (r.Value == 2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private int getNumberOn(int input, int num)
+        {
+            String s = input + "";
+
+            if (num > s.Length)
+            {
+                // error
+            }
+
+            return int.Parse(s[num] + "");
+        }
+
+        public long Answer(params long[] arguments)
+        {
+            int awnser = 0;
+
+            for(int i = input1; i < input2; i++)
+            {
+                if (passCriteria(i))
+                {
+                    awnser++;
+                    Console.WriteLine($"input {i} is correct");
+                    
+                }
+            }
+
+            return awnser;
+        }
+    }
+}

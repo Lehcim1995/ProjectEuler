83adf6e6f70db45cf3987d034d458c20e64ca89d
Jacobs <Michel.Jacobs@philips.com>
2019-12-06 17:01:56 +0100

    fixed cli

--- ProjectEuler/CLI/CLI.cs
+++ ProjectEuler/CLI/CLI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectEuler.Interfaces;
+using System;
 
 namespace ProjectEuler.CLI
 {
@@ -36,6 +37,49 @@ namespace ProjectEuler.CLI
             }
         }
 
+        private void ShowProblems()
+        {
+            for (int i = 1; i < 600; i++)
+            {
+
+                Type problem = Type.GetType("ProjectEuler.Problems.Problem" + i, false);
+
+                if (null != problem)
+                {
+                    Console.WriteLine($"Problem {i}");
+                    continue;
+                }
+            }
+        }
+
+        private void RunProblem()
+        {
+            Console.WriteLine("Which problem number?");
+            string num = Console.ReadLine().Trim();
+
+            Type problem = Type.GetType("ProjectEuler.Problems.Problem" + num, false);
+
+            if (null == problem)
+            {
+                Console.WriteLine("Problem not found");
+                return;
+            }
+
+            try
+            {
+                var pi = (IProblem)Activator.CreateInstance(problem);
+                Console.WriteLine("The answer is:");
+                Console.WriteLine(pi.Answer());
+            }
+            catch (NotImplementedException)
+            {
+                Console.WriteLine("Answer not implemented yet");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Interface not implemented in problem {num}");
+            }
+        }
 
         public void ProcessCommand()
         {
@@ -48,22 +92,23 @@ namespace ProjectEuler.CLI
                 // Show help
                 case Commands.Help:
                 case Commands.H:
-
+                    ListCommands();
                     break;
                 // Show all commands
                 case Commands.Commands:
-                    GetCommands();
+                    ListCommands();
+                    
                     break;
 
                 // List all problems
                 case Commands.List:
                 case Commands.L:
-                    ListCommands();
+                    ShowProblems();
                     break;
                 // Execute a problem
                 case Commands.Problem:
                 case Commands.P:
-
+                    RunProblem();
                     break;
                 default:
                     // No valid command found? try to guess otherwise give help commando. 
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -8,46 +8,14 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Cli cli = new Cli();;
+            Cli cli = new Cli();
+
+            Console.WriteLine("This is the Project euler commandline application");
+            Console.WriteLine("Type \"Commands\" to see all commands ");
 
             while (true)
             {
                 cli.ProcessCommand();
-
-                Console.WriteLine("Which problem number?");
-                string num = Console.ReadLine().Trim();
-
-
-                switch (num.ToLower()) //for quiting
-                {
-                    case "q":
-                    case "quit":
-                    case "exit":
-                        return;
-                }
-
-                Type problem = Type.GetType("ProjectEuler.Problems.Problem" + num, false);
-
-                if (null == problem)
-                {
-                    Console.WriteLine("Problem not found");
-                    continue;
-                }
-
-                try
-                {
-                    var pi = (IProblem) Activator.CreateInstance(problem);
-                    Console.WriteLine("The answer is:");
-                    Console.WriteLine(pi.Answer());
-                }
-                catch (NotImplementedException)
-                {
-                    Console.WriteLine("Answer not implemented yet");
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine($"Interface not implemented in problem {num}");
-                }
             }
         }
     }

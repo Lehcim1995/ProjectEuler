09bc0ae8b59db80b22dd7bca6f456007d8d0b9ec
Michel Jacobs <micheljacobs@hotmail.nl>
2019-12-22 16:40:30 +0100

    day 18 and day 17 part 2

---
+++ adventofcode2019/Days/Day18.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using adventofcode2019.Interfaces;
+
+namespace adventofcode2019.Days
+{
+    public class Day18 : IProblem
+    {
+
+
+        public long Answer(params long[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

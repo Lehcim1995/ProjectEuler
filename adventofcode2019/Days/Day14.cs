4f66db2c6f4f437227d43ffbadc7a4c4a081217e
Michel Jacobs <Michel.Jacobs@philips.com>
2020-01-31 15:35:42 +0100

    some day 14

--- adventofcode2019/Days/Day14.cs
+++ adventofcode2019/Days/Day14.cs
@@ -1,7 +1,9 @@
 ﻿using adventofcode2019.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,15 @@ namespace adventofcode2019.Days
 
         private string _ore = "ORE";
         private string _fuel = "FUEL";
+
+        
+        class Elemental
+        {
+            private string Name;
+            private Elemental[] requires;
+
+
+        }
 
         class Reaction
         {
@@ -27,29 +38,70 @@ namespace adventofcode2019.Days
                 // Set values based on shizzle
 
                 // 
+                int productLoc = reaction.IndexOf("=>");
 
+                string product = reaction.Substring(productLoc + 3);
+                Console.WriteLine(product);
+
+                string[] requirers = reaction.Substring(0, productLoc).Split(',');
+                foreach (var req in requirers)
+                {
+                    Console.WriteLine(" " + req);
+                }
 
                 throw new ArgumentException("Reaction is not in correct thingy");
             }
         }
 
-        Dictionary<String, Reaction> Reactions;
-        Dictionary<String, long> Resources;
+        Dictionary<string, Reaction> Reactions;
+        Dictionary<string, long> Resources;
+
+        Dictionary<string, List<string>> EndProducts;
+
 
         private void parseInput()
         {
+            List<String> lines = new List<string>();
+            string line;
 
+            // Read the file and display it line by line.  
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Files\day14_input.txt");
+
+            using (StreamReader file =
+                new StreamReader(path))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            EndProducts = new Dictionary<string, List<string>>();
+
+            foreach (var reaction in lines)
+            {
+                int productLoc = reaction.IndexOf("=>");
+
+                string product = reaction.Substring(productLoc + 3);
+                Console.WriteLine(product);
+
+                string[] requirers = reaction.Substring(0, productLoc).Split(',');
+                foreach(var req in requirers)
+                {
+                    Console.WriteLine(" " + req);
+                }
+            }
         }
 
         public long Answer(params long[] arguments)
         {
             // 
 
-            var r = Reactions[_ore];
+            //var r = Reactions[_ore];
             // loop though all the reactions til all reactions have reaced the ore
+            parseInput();
 
-
-            throw new NotImplementedException();
+            return 0;
         }
     }
 }
